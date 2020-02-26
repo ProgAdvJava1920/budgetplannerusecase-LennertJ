e512130e0a6bd24d4c78bf462b8e98709f983095
lennert <jans.lennert@gmail>
2020-02-26 09:16:55 +0100

    Added Logging

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -1,6 +1,20 @@
 package be.pxl.student;
 
+import be.pxl.student.entity.Account;
+import be.pxl.student.util.BudgetPlannerImporter;
+
+import java.util.List;
+
+
 public class BudgetPlanner {
 
+    public static void main(String[] args) {
+        BudgetPlannerImporter budgetPlannerImporter = new BudgetPlannerImporter("src/main/resources/account_payments.csv");
+
+        List<Account> accounts = (List<Account>) budgetPlannerImporter.readFile();
+
+        accounts.forEach(System.out::println);
+    }
 
 }
+//https://logging.apache.org/log4j/2.x/manual/appenders.html#RollingFileAppender
